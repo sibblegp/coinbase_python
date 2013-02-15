8703fc1e38c7681f9c5f51e3e9d0efdcbbf29a22
George Sibble <gsibble@gmail.com>
2013-02-15 15:36:19 -0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='subtledata',
+    version='0.0.1',
+    packages=['subtledata'],
+    url='http://dev.subtledata.com/libraries/python',
+    license='MIT',
+    author='George Sibble',
+    author_email='george.sibble@subtledata.com',
+    description='Integration Library for the Subtledata Platform',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Office/Business :: Financial :: Point-Of-Sale',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+)
