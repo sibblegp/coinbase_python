6c7a6a0b1557eacaad5d83124cc35105c595a49d
George Sibble <gsibble@gmail.com>
2013-03-21 22:25:59 -0700

    Logging and oAuth Adjustments

--- coinbase_oauth2/__init__.py
+++ coinbase_oauth2/__init__.py
@@ -8,6 +8,9 @@ import httplib2
 APP = Flask(__name__)
 APP.debug = True
 
+import logging
+logging.basicConfig()
+
 from secrets import CALLBACK_URL, CLIENT_ID, CLIENT_SECRET
 
 coinbase_client = OAuth2WebServerFlow(CLIENT_ID, 'all', 'all', 'http://www.paywithair.com/consumer_auth', auth_uri='https://www.coinbase.com/oauth/authorize', token_uri='https://www.coinbase.com/oauth/token')
@@ -15,7 +18,7 @@ coinbase_client = OAuth2WebServerFlow(CLIENT_ID, 'all', 'all', 'http://www.paywi
 @APP.route('/')
 def register_me():
 
-    auth_url = coinbase_client.step1_get_authorize_url('http://www.paywithair.com/consumer_auth')
+    auth_url = coinbase_client.step1_get_authorize_url()
 
     return render_template('register.jinja2', auth_url=auth_url)
 
@@ -24,11 +27,13 @@ def receive_token():
 
     oauth_code = request.args['code']
 
+    print oauth_code
+
     http = httplib2.Http(disable_ssl_certificate_validation=True)
 
-    token = coinbase_client.step2_exchange(oauth_code, http)
+    token = coinbase_client.step2_exchange(oauth_code, http=http)
 
     return make_response(token.to_json())
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=5010)
+    APP.run(host='0.0.0.0', port=80)
