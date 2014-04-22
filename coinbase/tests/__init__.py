9d1b1ab0570ec433b4fd4ea178f093c4a6050b1b
Chris Martin <ch.martin@gmail.com>
2014-04-22 02:32:25 -0400

    test with exchange_rates.json

--- coinbase/tests/__init__.py
+++ coinbase/tests/__init__.py
@@ -5,10 +5,16 @@ from sure import it, this, those, these
 import unittest
 from httpretty import HTTPretty, httprettified
 from decimal import Decimal
+import os.path
 
 from coinbase import CoinbaseAccount, CoinbaseAmount
 
 TEMP_CREDENTIALS = '''{"_module": "oauth2client.client", "token_expiry": "2014-03-31T23:27:40Z", "access_token": "c15a9f84e471db9b0b8fb94f3cb83f08867b4e00cb823f49ead771e928af5c79", "token_uri": "https://www.coinbase.com/oauth/token", "invalid": false, "token_response": {"access_token": "c15a9f84e471db9b0b8fb94f3cb83f08867b4e00cb823f49ead771e928af5c79", "token_type": "bearer", "expires_in": 7200, "refresh_token": "90cb2424ddc39f6668da41a7b46dfd5a729ac9030e19e05fd95bb1880ad07e65", "scope": "all"}, "client_id": "2df06cb383f4ffffac20e257244708c78a1150d128f37d420f11fdc069a914fc", "id_token": null, "client_secret": "7caedd79052d7e29aa0f2700980247e499ce85381e70e4a44de0c08f25bded8a", "revoke_uri": "https://accounts.google.com/o/oauth2/revoke", "_class": "OAuth2Credentials", "refresh_token": "90cb2424ddc39f6668da41a7b46dfd5a729ac9030e19e05fd95bb1880ad07e65", "user_agent": null}'''
+
+
+def read(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return f.read()
 
 
 class CoinBaseAmountTests(unittest.TestCase):
@@ -209,21 +215,15 @@ class CoinBaseLibraryTests(unittest.TestCase):
 
     @httprettified
     def test_exchange_rates(self):
+
         HTTPretty.register_uri(
             HTTPretty.GET,
             "https://coinbase.com/api/v1/currencies/exchange_rates",
-            body='{"gbp_to_usd":"1.648093","usd_to_gbp":"0.606762",'
-                 '"btc_to_usd":"460.074131","usd_to_btc":"0.002174",'
-                 '"gbp_to_btc":"0.003583","btc_to_gbp":"279.1555"}',
-            content_type='text/json',
-        )
+            body=read('exchange_rates.json'),
+            content_type='text/json')
+
         rates = self.account.exchange_rates
-        this(rates).should.be.an(dict)
-        this(rates).should.be.equal({
-            'gbp_to_usd': Decimal('1.648093'),
-            'usd_to_gbp': Decimal('0.606762'),
-            'btc_to_usd': Decimal('460.074131'),
-            'usd_to_btc': Decimal('0.002174'),
-            'gbp_to_btc': Decimal('0.003583'),
-            'btc_to_gbp': Decimal('279.1555')
-        })
+        this(rates['gbp_to_usd']).should.be.equal(Decimal('1.648093'))
+        this(rates['usd_to_btc']).should.be.equal(Decimal('0.002'))
+        this(rates['btc_to_usd']).should.be.equal(Decimal('499.998'))
+        this(rates['bdt_to_btc']).should.be.equal(Decimal('0.000026'))
